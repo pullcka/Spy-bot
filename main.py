1672121620dac784d5135d76dd174c47296f19e8
pullcka <pogrebnojartem4@gmail.com>
2026-07-18 12:59:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Бот {bot.user} запущен!")
+
+bot.run(os.getenv("TOKEN"))
