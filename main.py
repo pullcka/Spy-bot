1ed26e8c0f440fa3f964bf2a9527d829b0822291
pullcka <pogrebnojartem4@gmail.com>
2026-07-19 12:55:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import discord
 from discord.ext import commands
 import os
+from threading import Thread
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+Thread(target=run_web).start()
 
 intents = discord.Intents.default()
 intents.message_content = True
